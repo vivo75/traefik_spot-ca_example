0c8e4ad434d38af28c13db08038ae4b27466a034
Francesco Riosa <vivo75@gmail.com>
2023-04-19 18:10:39 +0000

    Add 2 servers answering to 3 different domains
    Two new images are created and traefik routes for the container is
    added.
    
    test with:
    curl --header 'Host:api1.test.net' 'http://localhost:80/'
    curl --header 'Host:api2.test.net' 'http://localhost:80/'
    curl --header 'Host:api3.test.net' 'http://localhost:80/'
    
    The answer for second container (which answer as api 2 and 3) is
    slightly different.
    
    It's also possible to query http://${HOST}/items/<number>?q=<string>

---
+++ api-server/app/main.py
@@ -0,0 +1,29 @@
+"""
+This is a simple test server using the amazing fastapi library
+
+Provided the server is reachable at api1.test.net
+use this test from command line:
+
+curl --header 'Host:api1.test.net' 'http://localhost:80/'
+"""
+from typing import Union
+import  pprint
+
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root(request: Request):
+    pp = pprint.PrettyPrinter(indent=2, depth=4)
+    return {
+        "ClientIP": request.client.host,
+        "URL": request.url,
+        "Headers": pp.pformat(request.headers),
+        "Params": pp.pformat(request.query_params),
+    }
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
